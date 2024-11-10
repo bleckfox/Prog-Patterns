91f7c8320e0a23349c80e5a48dce837ff6cdb2eb
bleckfox <79881790355@ya.ru>
2024-11-10 15:46:27 +0300

    init: Create Solution with Singleton instance

---
+++ ConsoleApp/Patterns/Creational/Singleton/SingletonLogger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp.Patterns.Creational.Singleton;
+
+public class SingletonLogger
+{
+    /// <summary>
+    /// Приватное поле для обращения к экземпляру
+    /// </summary>
+    private static SingletonLogger _instance;
+
+    /// <summary>
+    /// Приватный конструктор, чтобы не создавать снаружи
+    /// </summary>
+    private SingletonLogger() { }
+
+    /// <summary>
+    /// Метод для получения доступа к экземпляру
+    /// </summary>
+    public static SingletonLogger Instance
+    {
+        get
+        {
+            // same variant
+            //if (_instance == null)
+            //{
+            //    _instance = new SingletonLogger();
+            //}
+
+            //return _instance;
+
+            return _instance ??= new SingletonLogger();
+        }
+    }
+
+    public void Log(string message) => Console.WriteLine($"{DateTime.Now:G}: {message}");
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿
+// Singleton
+using ConsoleApp.Patterns.Creational.Singleton;
+
+SingletonLogger logger = SingletonLogger.Instance;
+SingletonLogger logger2 = SingletonLogger.Instance;
+
+logger.Log($"Message from {nameof(logger)}");
+logger2.Log($"Message from {nameof(logger2)}");
+
+Console.WriteLine(ReferenceEquals(logger, logger2));
+
